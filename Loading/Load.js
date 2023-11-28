f546a628361dfca029a75a33e58d39f53c352fc3
FernandoVarela25 <106282950+FernandoVarela25@users.noreply.github.com>
2023-11-28 14:54:05 -0600

    cambios

--- Loading/Load.js
+++ Loading/Load.js
@@ -1,14 +1,46 @@
 function loadDoc(url, nameFunction) {
+    loadContent(url, nameFunction);
     const xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {nameFunction(this);}
+    xhttp.onload = function() {
+        nameFunction(this);
+        loadStyles();
+    };
     xhttp.open("GET", url);
     xhttp.send();
 }
 
-function loadPortada(xhttp){document.getElementById("Content").innerHTML = xhttp.responseText;}
-function loadIntro(xhttp){document.getElementById("Content").innerHTML = xhttp.responseText;}
-function loadJustificacion(xhttp){document.getElementById("Content").innerHTML = xhttp.responseText;}
-function loadParametros(xhttp){document.getElementById("Content").innerHTML = xhttp.responseText;}
-function loadPruebas(xhttp){document.getElementById("Content").innerHTML = xhttp.responseText;}
-function loadConclu(xhttp){document.getElementById("Content").innerHTML = xhttp.responseText;}
-function loadFuentes(xhttp){document.getElementById("Content").innerHTML = xhttp.responseText;}
+function loadPortada(xhttp) {
+    document.getElementById("Content").innerHTML = xhttp.responseText;
+}
+
+function loadIntro(xhttp) {
+    document.getElementById("Content").innerHTML = xhttp.responseText;
+}
+
+function loadJustificacion(xhttp) {
+    document.getElementById("Content").innerHTML = xhttp.responseText;
+}
+
+function loadParametros(xhttp) {
+    document.getElementById("Content").innerHTML = xhttp.responseText;
+}
+
+function loadPruebas(xhttp) {
+    document.getElementById("Content").innerHTML = xhttp.responseText;
+}
+
+function loadConclu(xhttp) {
+    document.getElementById("Content").innerHTML = xhttp.responseText;
+}
+
+function loadFuentes(xhttp) {
+    document.getElementById("Content").innerHTML = xhttp.responseText;
+}
+
+function loadStyles() {
+    var link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href = "assets/styles.css";
+    document.head.appendChild(link);
+}
